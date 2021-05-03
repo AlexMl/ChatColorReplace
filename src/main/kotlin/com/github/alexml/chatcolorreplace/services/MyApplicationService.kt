94c3c62b77a6f9ee931bcc691491b07d00764017
GitHub Action <action@github.com>
2021-05-03 15:31:12 +0000

    Template cleanup

--- src/main/kotlin/com/github/alexml/chatcolorreplace/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/alexml/chatcolorreplace/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.alexml.chatcolorreplace.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.alexml.chatcolorreplace.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/alexml/chatcolorreplace/services/MyProjectService.kt
+++ src/main/kotlin/com/github/alexml/chatcolorreplace/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.alexml.chatcolorreplace.services
 
+import com.github.alexml.chatcolorreplace.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
